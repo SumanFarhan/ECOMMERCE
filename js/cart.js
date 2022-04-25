b769a129389ee2d6eedac5adc7040f3ad7973a8b
SumanFarhan <sumanfarhan2800@gmail.com>
2022-04-25 12:26:39 +0500

    cart.html,cart.js,cart.css,product.js,product.html

--- js/cart.js
+++ js/cart.js
@@ -1,57 +1,7 @@
-const product_total_amount=document.getElementById("total_amount");
-//decrement counter
-const decreament = () => {
-    var itemval = document.getElementById("textbox");
-    var itemprice = document.getElementById("itemprice");
-    console.log(itemprice)
-    if (itemval.value <= 0) {
-        itemval.value = 0;
+//total amount
+const product_total_amount=document.getElementById("total_amount");//total
 
-    }
-    else {
-    let products=localStorage.getItem("products");
-    const parsearr=JSON.parse(products)
-    parsearr.map((i)=>{
-    document.getElementById("titleget").innerHTML =i.title;
-    document.getElementById("itemprice").innerHTML=i.price;
-        itemval.value = parseInt(itemval.value) - 1;
-        itemval.style.background = "#fff";
-        itemval.style.color = '#000';
-        let item=itemval.value;
-        let price=i.price;
-        let decrement=item*price;
-        itemprice.innerHTML =decrement; 
-        product_total_amount.innerHTML=decrement;
 
-    })
-    }
-}
-
-//Increment counter
-const increament = () => {
-    var itemval = document.getElementById("textbox");
-    var itemprice = document.getElementById("itemprice");
-    if (itemval.value >= 5) {
-        itemval.value = 5;
-        alert("max 5 allowed");
-        itemval.style.background = "red";
-        itemval.style.color = '#fff';
-    }
-    else {
-let products=localStorage.getItem("products");
-const parsearr=JSON.parse(products)
-parsearr.map((i)=>{
-    document.getElementById("titleget").innerHTML =i.title;
-   document.getElementById("itemprice").innerHTML=i.price;
-itemval.value = parseInt(itemval.value) + 1;
-console.log(itemval.value,"quantity")
-   itemprice.innerHTML = itemval.value * i.price;
-   product_total_amount.innerHTML=itemval.value * i.price;
-})
-
-    }
-    
-}
 
 // local storage delete
 const delete_product=(itemIndex)=>{
@@ -60,18 +10,31 @@ const delete_product=(itemIndex)=>{
     parsearr.splice(itemIndex,1);
     localStorage.setItem('products',JSON.stringify(parsearr));
 }
+console.log("hhhh")
 
 // local storage Add
 let products=localStorage.getItem("products");
 const parsearr=JSON.parse(products);
 parsearr.map((i)=>{
-    let cart=document.getElementById("titleget");
-    let li=document.createElement('li');
-    let text=document.createTextNode(i.title);
-     li.appendChild(text);
-    cart.appendChild(li);
+    //title 
+    let title=document.getElementById("titleget");
+    let h5=document.createElement('h5');
+    h5.style.marginTop="2px";
+    let span=document.createElement('span');
+    let p=document.createTextNode("$")
+    let value=document.createTextNode(i.price);
+    span.setAttribute("class","amount");
+    span.setAttribute("id","itemprice");
+    let titletext=document.createTextNode(i.title);
+    let icon= document.createElement('i');
+    icon.style.paddingRight="5px";
+    icon.setAttribute("class","fa fa-trash-o");
+    icon.setAttribute("onclick","delete_product(this)")
+    h5.appendChild(icon);
+    h5.appendChild(titletext);
+    title.appendChild(h5)
+    span.appendChild(p);
+    span.appendChild(value);
+    h5.appendChild(span);
+       
 })
-
-
-
-
